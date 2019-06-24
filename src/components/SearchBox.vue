4bd6b2b2cccea22c939a5d732c6c927e2b76dc52
IMAD ABOULHOUDA <imadaboulhouda1@gmail.com>
2019-06-24 12:44:47 +0100

    just playing

---
+++ src/components/SearchBox.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+       <form>
+        <input type="text" v-model="search" @keyup="searching()"/>
+         </form>
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return{
+            search:'',
+        };
+    },
+    created(){
+
+    },
+    methods:{
+        searching(){
+            this.$emit('searching',this.search);
+        },
+        loading(){
+
+        }
+    }
+}
+</script>
